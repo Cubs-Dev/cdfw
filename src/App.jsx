63e470ee374dc43cc367356b9c0ecca622ae387b
Cubs-Dev <cubsdevelopment@gmail.com>
2025-02-27 13:13:18 +0100

    CCCCCCCCCC

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Page
-import AjoutDon from "./page/ajoutdon";
 import Ajoutdemonde from './page/ajoutdemonde';
 
 function App() {
     return (
         <Router>
             <Routes>
-            <Route path="/test" element={<AjoutDon/>}/>
-            //<Route path="/assossiation" element={<Ajoutdemonde/>}/>*/
+            <Route path="/test" element={<Ajoutdemonde/>}/>
             </Routes>
         </Router>
     );
